da9906945d4d1163896506437ddbf01822afb2bc
sidhu8300 <64597209+sidhu8300@users.noreply.github.com>
2022-04-19 10:11:28 +0530

    Create Solution4.java

---
+++ Solution4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Solution44{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int tmp=0,a=0;
+while(sc.hasNext()){
+ a=sc.nextInt();
+if(a>0 && a<1000){
+	tmp+=a;
+}
+
+
+}
+sc.close();
+System.out.print(a);
+
+}
+
+
+}
+
+
